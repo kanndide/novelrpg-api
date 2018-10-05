c0674b7ed3861c4eb2b1541a850a6900d56bace8
kanndide <kanndide@gmail.com>
2018-10-05 20:24:22 +0300

    added email validation for errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   
-  devise :database_authenticatable,
-  		 :registerable,
-         :jwt_authenticatable,
-         jwt_revocation_strategy: JWTBlacklist
+validates :email, uniqueness: { case_sensitive: false }
+
+devise :database_authenticatable,
+  	   :registerable,
+       :jwt_authenticatable,
+       jwt_revocation_strategy: JWTBlacklist
 
 end
