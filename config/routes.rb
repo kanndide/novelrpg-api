8e67f51ce8171bb0c8bac2203cf66def6b3f0df4
kanndide <kanndide@gmail.com>
2018-10-05 12:46:22 +0300

    devise routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             path: '',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+             controllers: {
+               sessions: 'sessions',
+               registrations: 'registrations'
+  			}
   root 'index#home'
 end
