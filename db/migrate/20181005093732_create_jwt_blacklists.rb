a7386ecbad52e479fbe2f834f6be9758578bd8e0
kanndide <kanndide@gmail.com>
2018-10-05 13:21:45 +0300

    fixed migrations for user

--- db/migrate/20181005093732_create_jwt_blacklists.rb
+++ db/migrate/20181005093732_create_jwt_blacklists.rb
@@ -1,6 +1,6 @@
 class CreateJwtBlacklists < ActiveRecord::Migration[5.0]
   def change
-    create_table :jwt_blacklists do |t|
+    create_table :jwt_blacklist do |t|
       t.string :jti, null: false
     end
     add_index :jwt_blacklist, :jti
