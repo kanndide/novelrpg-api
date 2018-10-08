634bd2f53e9d6ab08c2b57fb732a0b685d1c3dc7
kanndide <kanndide@gmail.com>
2018-10-08 14:28:21 +0300

    removed has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   
 validates :email, uniqueness: { case_sensitive: false }
-has_secure_password
+# has_secure_password
 
 devise :database_authenticatable,
   	   :registerable,
