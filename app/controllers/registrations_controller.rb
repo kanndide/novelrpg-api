d8238bf661d656c94798bf75cfef2de9bd77ef13
kanndide <kanndide@gmail.com>
2018-10-08 14:28:03 +0300

    params defined to allow params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,8 +2,15 @@ class RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
   def create
-    build_resource(sign_up_params)
+    build_resource(reg_params)
     resource.save
     render_resource(resource)
   end
+
+  private
+
+  def reg_params
+  	params.require(:registration).permit(:email, :password)
+  end
+
 end
