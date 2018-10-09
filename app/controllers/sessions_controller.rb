680dfe77540f303acdf5924b19954004923e867c
kanndide <kanndide@gmail.com>
2018-10-09 15:39:23 +0300

    changed create route to return correctly formatted object

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,14 @@
 class SessionsController < Devise::SessionsController
   respond_to :json
 
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    render json: { user: resource, token: request.headers["warden-jwt_auth.token"] }
+  end
+
   private
 
   def respond_with(resource, _opts = {})
