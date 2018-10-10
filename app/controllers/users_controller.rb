0f01974a261eedb396d4aca23a73c6f2c7a75ccd
kanndide <kanndide@gmail.com>
2018-10-10 08:13:35 +0300

    show path created for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,9 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!
+
+	def show
+		@user = User.find(params[:id])
+		render :json => @user.to_json(:include => {:characters => { :include => :skill_set}})
+	end
 
 end
