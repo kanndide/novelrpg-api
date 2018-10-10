1060e6c268e1419f9620a499d9d956ab6fef65a0
kanndide <kanndide@gmail.com>
2018-10-10 08:12:08 +0300

    character seed for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Character.create(name: "Doc Holiday", chapter: 0, scene: 0, gold: 50, hp: 100, user_id: 1)
+SkillSet.create(swordsman: 5, charmer: 5, athlete: 5, survivor: 5, character_id: 1)
